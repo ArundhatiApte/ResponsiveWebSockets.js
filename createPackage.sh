3d207ae44126d0b06f8ed0dda7f57e948b788c9f
Arundhati Apte <97678771+ArundhatiApte@users.noreply.github.com>
2022-04-28 16:20:02 +0300

    change creating package script

--- createPackage.sh
+++ createPackage.sh
@@ -3,12 +3,6 @@
 rm ResponsiveWebSockets.package.tar.gz
 
 tar -czvf ResponsiveWebSockets.package.tar.gz \
-  --exclude='node_modules'\
-  --exclude='docs'\
-  --exclude='.git'\
-  --exclude='examples'\
-  --exclude=".gitignore"\
   --exclude='test*'\
   --exclude='tests.js'\
-  --exclude='createPackage.sh'\
-  .
+  LICENCE package.json README.md src
