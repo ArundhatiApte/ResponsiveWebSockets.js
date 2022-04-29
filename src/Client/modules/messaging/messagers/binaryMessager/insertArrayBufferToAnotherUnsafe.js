b752449d43231a3de139fb2fa6128660989d892c
Arundhati Apte <97678771+ArundhatiApte@users.noreply.github.com>
2022-04-29 15:47:58 +0300

    fix bug (endianess in client binary messager)

--- src/Client/modules/messaging/messagers/binaryMessager/insertArrayBufferToAnotherUnsafe.js
+++ src/Client/modules/messaging/messagers/binaryMessager/insertArrayBufferToAnotherUnsafe.js
@@ -1,15 +1,15 @@
 "use strict";
 
 const insertArrayBufferToAnotherUnsafe = function(sourceArrayBuffer, startIndexInSource, insertedArrayBuffer) {
-  const sourceBytes = new Uint8Array(sourceArrayBuffer),
-        insertedBytes = new Uint8Array(insertedArrayBuffer),
-        countOfInsertedBytes = insertedBytes.length,
-        afterLastIndex = startIndexInSource + countOfInsertedBytes - 1;
+  const sourceData = new DataView(sourceArrayBuffer);
+  const insertedData = new DataView(insertedArrayBuffer);
+  const countOfInsertedBytes = insertedArrayBuffer.byteLength;
 
-  let indexInSource = startIndexInSource,
-      indexInInserted = 0;
+  let indexInSource = startIndexInSource;
+  let indexInInserted = 0;
+
   for (; indexInInserted < countOfInsertedBytes;) {
-    sourceBytes[indexInSource] = insertedBytes[indexInInserted];
+    sourceData.setUint8(indexInSource, insertedData.getUint8(indexInInserted));
     indexInSource += 1;
     indexInInserted += 1;
   }
