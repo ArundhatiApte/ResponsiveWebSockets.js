27793e7ac2b96994f7590880e2d954dfa8ba44b2
Arundhati Apte <97678771+ArundhatiApte@users.noreply.github.com>
2022-07-14 19:03:06 +0300

    fix creatingPackage script and version of uWebSockets.js

--- project/createPackage.sh
+++ project/createPackage.sh
@@ -3,6 +3,6 @@
 rm ResponsiveWebSockets.package.tar.gz
 
 tar -czvf ResponsiveWebSockets.package.tar.gz \
+  --transform='s,^,package/,'\
   --exclude='test*'\
-  --exclude='tests.js'\
   ./../LICENCE package.json ./../README.md src
